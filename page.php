d1b8c147a83fc79e90e9c90f5ea16f31fb1afb96
Al-Rayyan Abdurahman <rayyana00@gmail.com>
2020-01-10 16:03:59 +0800

    Page

---
+++ page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<?php 
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        single_post_title();
+        the_content();
+    endwhile; else: ?>
+    <p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
